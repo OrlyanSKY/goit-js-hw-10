4349cbdfd679ec294ce1ef87a8269a8842ca43ca
OrlyanSKY <100310036+OrlyanSKY@users.noreply.github.com>
2022-08-21 12:22:26 +0000

    Deploying to gh-pages from @ OrlyanSKY/goit-js-hw-10@6e31e41062c661d6427737eb3fa36cdb417419eb 🚀

--- index.dcc36bfc.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("#search-box").addEventListener("input",(e=>{let t=e.target.value;fetch(`https://restcountries.com/v3.1/name/${t}`).then((e=>e.json())).then((e=>console.log(e)))}));
-//# sourceMappingURL=index.dcc36bfc.js.map
