985897eddbaca24823d6c080bc7365b9c235a0b8
Denis Orlyansky <000slim001@gmail.com>
2022-08-22 17:10:58 +0300

    completed

--- src/index.js
+++ src/index.js
@@ -8,34 +8,44 @@ import { fetchCountries } from './js/fetchCountries';
 const DEBOUNCE_DELAY = 500;
 
 const countryInfo = document.querySelector('.country-info');
-const countryList = document.querySelector('country-list');
+const countryList = document.querySelector('.country-list');
 const input = document.querySelector('#search-box');
 
 input.addEventListener('input', debounce(onInputChange, DEBOUNCE_DELAY));
 
 function onInputChange(evt) {
-  let inputValue = evt.target.value;
+  let inputValue = evt.target.value.trim(); //санитизацию введенной строки
 
   if (inputValue) {
     fetchCountries(inputValue).then(renderMarcup).catch(onError);
   }
+  if (!inputValue) {
+    clearMarcup();
+  }
 }
 
 function renderMarcup(data) {
-  countryInfo.innerHTML = '';
+  clearMarcup();
+
   if (data.length > 10) {
     Notify.info('Too many matches found. Please enter a more specific name.');
     return;
   }
   if (data.length >= 2) {
     data.forEach(elem => {
-      countryInfo.innerHTML += someCountriesMarkup(elem);
+      countryList.innerHTML += someCountriesMarkup(elem);
     });
-  } else if (data.length === 1) {
+  }
+  if (data.length === 1) {
     countryInfo.innerHTML = countryMarkup(data[0]);
   }
 }
 
 function onError() {
   Notify.failure('Oops, there is no country with that name');
+}
+
+function clearMarcup() {
+  countryInfo.innerHTML = '';
+  countryList.innerHTML = '';
 }
